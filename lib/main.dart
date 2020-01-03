633d25c76b3dee73ec7c6722f3d912e12a2da7e5
櫻井 貴央 <shibadog1121@gmail.com>
2020-01-03 13:06:07 +0900

    StatefulWidgetとStateは異なるライフサイクルをもつ Widgetは現在のstateによる表示をおこなうための一時的なオブジェクトであり、stateはbuild()メソッドが呼ばれる間も保持される継続的なオブジェクトである 複雑なアプリケーションになるとウィジェットヒエラルキーの異なる部分がそれぞれに異なる関心を持つことがあるので、適切に分割して上げる必要がある。
    571317bac5ff6c65b5cc91f3e6ca9051b67a56ee で実装したものを、現在のカウントを表示する役割と、カウントアップする役割に分けて実装したのがこのコミットである。
    
    flutterでは変更されたことを親のコンポーネントに知らせるときにはコールバックが、子のコンポーネントに知らせるときはStatelessWidgetにcurrent stateの値を渡すことで実現する

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,32 @@ title: 'Flutter Tutorial',
 home: Counter(),
 ));
 
+class CounterDisplay extends StatelessWidget {
+  CounterDisplay({this.count});
+
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Count: $count');
+  }
+}
+
+class CounterIncrementor extends StatelessWidget {
+  CounterIncrementor({this.onPressed});
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: onPressed,
+      child: Text('Increment'),
+    );
+  }
+}
 
 class Counter extends StatefulWidget {
-  // This class is the configuration for the state. It holds the
-  // values (in this case nothing) provided by the parent and used by the build
-  // method of the State. Fields in a Widget subclass are always marked "final".
-
   @override
   _CounterState createState() => _CounterState();
 }
@@ -20,32 +40,15 @@ class _CounterState extends State<Counter> {
 
   void _increment() {
     setState(() {
-      // This call to setState tells the Flutter framework that
-      // something has changed in this State, which causes it to rerun
-      // the build method below so that the display can reflect the
-      // updated values. If you change _counter without calling
-      // setState(), then the build method won't be called again,
-      // and so nothing would appear to happen.
-      _counter++;
+      ++_counter;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called,
-    // for instance, as done by the _increment method above.
-    // The Flutter framework has been optimized to make rerunning
-    // build methods fast, so that you can just rebuild anything that
-    // needs updating rather than having to individually change
-    // instances of widgets.
-    return Row(
-      children: <Widget>[
-        RaisedButton(
-          onPressed: _increment,
-          child: Text('Increment'),
-        ),
-        Text('Count: $_counter'),
-      ],
-    );
+    return Row(children: <Widget>[
+      CounterIncrementor(onPressed: _increment),
+      CounterDisplay(count: _counter),
+    ]);
   }
 }
