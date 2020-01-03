7287409feeb093d1eb1504b1cc6fe0b525f9f01d
櫻井 貴央 <shibadog1121@gmail.com>
2020-01-03 12:22:29 +0900

    https://flutter.dev/docs/development/ui/widgets-intro#handling-gestures
    GestureDetector widgetを使えばユーザーの操作(タップとかどらっぐとか)をコールバックを指定することでハンドリングすることができる。
    多くのウィジェットがGestureDetectorを使って、コールバックを提供している

--- lib/main.dart
+++ lib/main.dart
@@ -2,38 +2,28 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
 title: 'Flutter Tutorial',
-home: TutorialHome(),
+home: MyButton(),
 ));
 
 
-class TutorialHome extends StatelessWidget {
+class MyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Scaffold is a layout for the major Material Components.
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
+    return GestureDetector(
+      onTap: () {
+        print('MyButton was tapped!');
+      },
+      child: Container(
+        height: 36.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.lightGreen[500],
         ),
-        title: Text('Example title'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          ),
-        ],
-      ),
-      // body is the majority of the screen.
-      body: Center(
-        child: Text('Hello, world!'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Add', // used by assistive technologies
-        child: Icon(Icons.add),
-        onPressed: null,
+        child: Center(
+          child: Text('Engage'),
+        ),
       ),
     );
   }
