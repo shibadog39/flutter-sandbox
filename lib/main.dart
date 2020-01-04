49d99e0ca524431367ed98ab11d94dd15b08a8cf
櫻井 貴央 <shibadog1121@gmail.com>
2020-01-04 20:08:47 +0900

    https://flutter.dev/docs/development/ui/layout#non-material-apps
    By default a non-Material app doesn’t include an AppBar, title, or background color. If you want these features in a non-Material app, you have to build them yourself

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,16 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter layout demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter layout demo'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
+    return Container(
+      decoration: BoxDecoration(color: Colors.white),
+      child: Center(
+        child: Text(
+          'Hello World',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 32,
+            color: Colors.black87,
+          ),
         ),
       ),
     );
