cd18cfbb263f72cf2a19ee7f9854b2622e6022c7
櫻井 貴央 <shibadog1121@gmail.com>
2020-01-04 20:38:41 +0900

    https://flutter.dev/docs/development/ui/layout#nesting-rows-and-columns

--- lib/main.dart
+++ lib/main.dart
@@ -9,17 +9,109 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final ratings = Container(
+    padding: EdgeInsets.all(20),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(Icons.star, color: Colors.green[500]),
+            Icon(Icons.star, color: Colors.green[500]),
+            Icon(Icons.star, color: Colors.green[500]),
+            Icon(Icons.star, color: Colors.black),
+            Icon(Icons.star, color: Colors.black),
+          ],
+        ),
+        Text(
+          '170 Reviews',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w800,
+            fontFamily: 'Roboto',
+            letterSpacing: 0.5,
+            fontSize: 20,
+          ),
+        ),
+      ],
+    ),
+  );
+
+
+// DefaultTextStyle.merge() allows you to create a default text
+// style that is inherited by its child and all subsequent children.
+  final iconList = DefaultTextStyle.merge(
+    style: TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontFamily: 'Roboto',
+      letterSpacing: 0.5,
+      fontSize: 18,
+      height: 2,
+    ),
+    child: Container(
+      padding: EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            children: [
+              Icon(Icons.kitchen, color: Colors.green[500]),
+              Text('PREP:'),
+              Text('25 min'),
+            ],
+          ),
+          Column(
+            children: [
+              Icon(Icons.timer, color: Colors.green[500]),
+              Text('COOK:'),
+              Text('1 hr'),
+            ],
+          ),
+          Column(
+            children: [
+              Icon(Icons.restaurant, color: Colors.green[500]),
+              Text('FEEDS:'),
+              Text('4-6'),
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(color: Colors.white),
-      child: Center(
-        child: Text(
-          'Hello World',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            fontSize: 32,
-            color: Colors.black87,
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter layout demo'),
+        ),
+        body: Center(
+          child: Container(
+            margin: EdgeInsets.fromLTRB(0, 40, 0, 30),
+            height: 600,
+            child: Card(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 440,
+                    child: Container(
+                      padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
+                      child: Column(
+                        children: [
+                          ratings,
+                          iconList,
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
