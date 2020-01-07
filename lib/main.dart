5e26d55200fbb27efc9e913e4bea9dacaf91c9ca
櫻井 貴央 <shibadog1121@gmail.com>
2020-01-07 09:16:40 +0900

    A mix-and-match approach https://flutter.dev/docs/development/ui/interactive#a-mix-and-match-approach
    The developer cares whether the box is active. The developer probably doesn’t care how the highlighting is managed, and prefers that the tap box handles those details.

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
           title: Text('Flutter Demo'),
         ),
         body: Center(
-          child: ParentWidget(),
+          child: ParentWidgetC(),
         ),
       ),
     );
@@ -70,12 +70,12 @@ class _TapboxAState extends State<TapboxA> {
 
 //------------------------ ParentWidget --------------------------------
 
-class ParentWidget extends StatefulWidget {
+class ParentWidgetB extends StatefulWidget {
   @override
-  _ParentWidgetState createState() => _ParentWidgetState();
+  _ParentWidgetBState createState() => _ParentWidgetBState();
 }
 
-class _ParentWidgetState extends State<ParentWidget> {
+class _ParentWidgetBState extends State<ParentWidgetB> {
   bool _active = false;
 
   void _handleTapboxChanged(bool newValue) {
@@ -122,6 +122,102 @@ class TapboxB extends StatelessWidget {
         height: 200.0,
         decoration: BoxDecoration(
           color: active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+      ),
+    );
+  }
+}
+
+// ParentWidgetC manages the state for TapboxC.
+
+//---------------------------- ParentWidget ----------------------------
+
+class ParentWidgetC extends StatefulWidget {
+  @override
+  _ParentWidgetCState createState() => _ParentWidgetCState();
+}
+
+class _ParentWidgetCState extends State<ParentWidgetC> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TapboxC(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+//----------------------------- TapboxC ------------------------------
+
+class TapboxC extends StatefulWidget {
+  TapboxC({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  _TapboxCState createState() => _TapboxCState();
+}
+
+class _TapboxCState extends State<TapboxC> {
+  bool _highlight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+  }
+
+  Widget build(BuildContext context) {
+    // This example adds a green border on tap down.
+    // On tap up, the square changes to the opposite state.
+    return GestureDetector(
+      onTapDown: _handleTapDown, // Handle the tap events in the order that
+      onTapUp: _handleTapUp, // they occur: down, up, tap, cancel
+      onTap: _handleTap,
+      onTapCancel: _handleTapCancel,
+      child: Container(
+        child: Center(
+          child: Text(widget.active ? 'Active' : 'Inactive',
+              style: TextStyle(fontSize: 32.0, color: Colors.white)),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color:
+          widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+          border: _highlight
+              ? Border.all(
+            color: Colors.teal[700],
+            width: 10.0,
+          )
+              : null,
         ),
       ),
     );
