6aff03ed70e487fc1a38621e77b619b5139e6874
櫻井 貴央 <shibadog1121@gmail.com>
2020-01-07 09:08:14 +0900

    The parent widget manages the widget’s state https://flutter.dev/docs/development/ui/interactive#the-parent-widget-manages-the-widgets-state

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,28 @@ void main() {
   );
 }
 
+//------------------------- MyApp ----------------------------------
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+        ),
+        body: Center(
+          child: ParentWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+// TapboxA manages its own state.
+
+//------------------------- TapboxA ----------------------------------
 class TapboxA extends StatefulWidget {
   TapboxA({Key key}) : super(key: key);
 
@@ -44,19 +66,62 @@ class _TapboxAState extends State<TapboxA> {
   }
 }
 
-//------------------------- MyApp ----------------------------------
+// ParentWidget manages the state for TapboxB.
 
-class MyApp extends StatelessWidget {
+//------------------------ ParentWidget --------------------------------
+
+class ParentWidget extends StatefulWidget {
+  @override
+  _ParentWidgetState createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Demo'),
+    return Container(
+      child: TapboxB(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+//------------------------- TapboxB ----------------------------------
+
+class TapboxB extends StatelessWidget {
+  TapboxB({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            active ? 'Active' : 'Inactive',
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
         ),
-        body: Center(
-          child: TapboxA(),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: active ? Colors.lightGreen[700] : Colors.grey[600],
         ),
       ),
     );
